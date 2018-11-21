bf0720ee2b10c9195c5d2b37b145411b10fb1984
Mark Wissink <mwissink.b@gmail.com>
2018-11-21 13:26:10 -0500

    Fixed SQL issues in the Leaderboard resource

--- KnightRanker_RESTful_Prototype/src/main/java/edu/calvin/cs262/pilot/knight_ranker/LeaderboardResource.java
+++ KnightRanker_RESTful_Prototype/src/main/java/edu/calvin/cs262/pilot/knight_ranker/LeaderboardResource.java
@@ -6,10 +6,12 @@ import com.google.api.server.spi.config.Named;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import static com.google.api.server.spi.config.ApiMethod.HttpMethod.GET;
 
 public class LeaderboardResource {
+    private static final Logger log = Logger.getLogger(LeaderboardResource.class.getName());
     /**
      * GET
      * Gets the leaderboard for a given sport
@@ -23,9 +25,11 @@ public class LeaderboardResource {
         ResultSet resultSet = null;
         List<PlayerRank> result = new ArrayList<>();
         try {
+            log.info("Sport:" + sport);
             connection = DriverManager.getConnection(System.getProperty("cloudsql"));
             statement = connection.createStatement();
             resultSet = selectPlayerRanks(statement, sport);
+            log.info("Results: " + resultSet.toString());
             while (resultSet.next()) {
                 PlayerRank p = new PlayerRank(
                         Integer.parseInt(resultSet.getString(1)),
@@ -58,11 +62,11 @@ public class LeaderboardResource {
      */
     private ResultSet selectPlayerRanks(Statement statement, String sport) throws SQLException {
         return statement.executeQuery(
-                String.format("SELECT rank, emailAddress" +
-                                "FROM Player, Sport, Follow" +
-                                "WHERE Sport.name = '%s'" +
-                                "AND Follow.sportID = Sport.id" +
-                                "AND Follow.playerID = Player.id" +
+                String.format("SELECT rank, emailAddress " +
+                                "FROM Player, Sport, Follow " +
+                                "WHERE Sport.name = '%s' " +
+                                "AND Follow.sportID = Sport.id " +
+                                "AND Follow.playerID = Player.id " +
                                 "ORDER BY rank ASC",
                     sport
                 )
